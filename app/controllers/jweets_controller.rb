b516ecdf61addcd7a0021c89c1491f6e27963d5c
TonbiNeko <junkomikami312nz@gmail.com>
2020-12-08 22:59:04 +0900

    creare edit page
    編集画面の作成

--- app/controllers/jweets_controller.rb
+++ app/controllers/jweets_controller.rb
@@ -1,5 +1,5 @@
 class JweetsController < ApplicationController
-  before_action :set_jweet, only: [:show]
+  before_action :set_jweet, only: [:show, :edit]
   def index
     @jweets = Jweet.all
   end
@@ -11,6 +11,8 @@ class JweetsController < ApplicationController
     redirect_to new_jweet_path
   end
   def show
+  end
+  def edit
   end
   private
   def jweet_params
